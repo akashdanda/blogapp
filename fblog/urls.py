8b966c8703ee54fe43d36427bfdb0ed17f69d058
Akash Danda <mindstormakash@gmail.com>
2022-07-25 13:42:30 -0700

    changelink

--- fblog/urls.py
+++ fblog/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home),
+    path('',home),
     path('blog/',blogposts),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
